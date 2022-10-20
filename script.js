b12a0511782009fe955d93697d0279c09c364bb1
Melanie Roberts <robertsmelaniecc@gmail.com>
2022-10-19 20:58:13 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,53 @@
+// Loops Exercise
+// 1
+for(let i = 1; i < 8; i++){
+    console.log(i);
+}
+1
+2
+3
+4
+5
+6
+7
+// 2
+for(let i = 5; i < 26; i = i + 4){
+    console.log(i);
+}
+5
+9
+13
+17
+21
+25
+// 3a
+const wizards = ['Harry Potter', 'Herminone Granger', 'Ron Weasley'];
+//3b
+for (let i = 0; i < wizards.length; i++) {
+    console.log(wizards[i]);
+    
+}
+Harry Potter
+Herminone Granger
+Ron Weasley
+// 4a
+let harryPotterMovies = 0;
+// 4b
+while (harryPotterMovies < 9) {
+    console.log(harryPotterMovies);
+    harryPotterMovies++;
+    
+}
+//4c
+0
+1
+2
+3
+4
+5
+6
+7
+8
+
+// Bonus
+// 5
